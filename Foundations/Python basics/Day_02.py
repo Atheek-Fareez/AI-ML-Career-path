1b06c3a8f3807ef46fa89928c98609bf0eb82009
Atheek-Fareez <Atheekfareez47@gmail.com>
2025-11-24 18:55:07 +0530

    just finished pyhon booleans lesson

--- Foundations/Python basics/Day_02.py
+++ Foundations/Python basics/Day_02.py
@@ -15,3 +15,27 @@ if b > a :
 else:
     print(" b is not greater then a  ")
 
+# Evaluate Values and Variables
+# The bool() function allows you to evaluate any value, and give you True or False in return,
+
+''' Step 1: bool("Hello")
+
+"Hello" is a string.
+Python checks if the string is empty or not:
+Empty string "" → False
+Any non-empty string → True
+"Hello" is non-empty, so: True will be output'''
+
+print(bool("Hello"))
+
+
+''' Step 2: bool(15) 
+
+15 is an integer.
+Python checks if the number is zero or not:
+0 → False
+Any non-zero number → True
+15 is non-zero, so: True will be output '''
+
+print(bool(15))
+
